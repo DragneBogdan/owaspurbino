b4e67bd0199f357eae007248e41998b462526ea6
DragneBogdan <b.dragne@campus.uniurb.it>
2023-03-25 14:12:21 +0100

    Inserimento funzioni di i/o
    E calcolato il tempo che ci impiegano i thread a terminare

--- socket/creazioneSocket.py
+++ socket/creazioneSocket.py
@@ -7,8 +7,8 @@ import requests
 import time 
 from time import perf_counter
 import threading 
-from threading import Thread, Lock
-
+from threading import Lock
+ 
 # def di thread - cosa fa il thread 
 def ex_thread():
     
@@ -19,36 +19,66 @@ def ex_thread():
         # acquisisco il lock 
         lock.acquire()
         
+        # calcolo il tempo 
+    
         print("Processing...")
         r = requests.get("http://localhost/wordpress")
-    finally:
-        lock.release()
         
-def comando(s):
+    finally:
+        # rilascio il lock 
+        lock.release()
+          
+# COMANDO 
+def comando(cmd):
     while True:
         comando = input("-> ")
         if comando == "exit":
             print("Disconnessione in corso...")
-            s.close()
-            time.sleep(2)
-            sys.exit()
+            cmd.close()
+            time.sleep(0.2)
+            sys.exit() 
+            
+        # se il comando è go acquisisco i nuovamente i valori 
+        if comando == "go":
+            acquisizione(cmd)
         
 def connessione_server(indirizzo_server):
     try:
-        s = socket.socket() # creazione socket
+        
+        # creazione socket
+        s = socket.socket()
         s.connect(indirizzo_server)
         print(f"Connessione al server {indirizzo_server} stabilita")
-       
+
+        # posso acquisire i valori
+        comando(s)
     except socket.error as errore:
         print(f"Connessione al server non riuscita")
         
         # esco 
         sys.exit()
     
-    n_THREAD = 800
+def acquisizione(s):
+    try:
+        
+        # acquisizione del numero di richieste
+        acquisizione = input("Inserire il numero di richieste: ")
+        n_THREAD = int(acquisizione)
+        
+        # acquisizione dell'intervallo di tempo 
+        acq_Time = input("Inserire l'intervallo di tempo(nel formato 0.#): ")
+        time_To_sleep = float(acq_Time)
+        
+    except:
+        print("Inserire valori coerenti!")   
+        sys.exit
+    
+    # array di thread che effettuano la richiesta
     threads = []
     i = 0
-    for x in range(1,n_THREAD):
+    
+    # il ciclo for inserisce i thread nell'array e li fa partire
+    for x in range(0,n_THREAD):
         t = threading.Thread(target = ex_thread)
         
         # aggiungo il thread
@@ -57,21 +87,34 @@ def connessione_server(indirizzo_server):
         # avvio il thread
         t.start()
         
+        # incremento il numero di richieste 
         i += 1
         print("Richieste: ", i)
-        time.sleep(0.3)
         
+        # prima di fare un'altra richiesta attende un certo t
+        time.sleep(time_To_sleep)
+        
+    # verifico quanto tempo ci impiegano per terminare 
+    start = perf_counter()
+    
     # mi metto in attesa della loro terminazione 
     for th in threads:
         th.join()
+        
+    stop = perf_counter()
     
-    # messaggio 
+    print("Tempo impiegato: ",(stop - start))
+        
+    # ritorno ad acquisire il comando
     comando(s)
-    
+        
 # esecuzione passando il mio indirizzo locale
 if __name__ == "__main__":
+    print("")
+    print("Comandi:")
+    print("--------")
+    print("Go: per avviare il programma")
+    print("exit: per uscire")
+    print("")
     connessione_server(("localhost",80))
-    
-    
-#https://www.digitalocean.com/community/tutorials/how-to-use-threadpoolexecutor-in-python-3
     
